26787eabe6c13e6409a298a96d23eaf1b10b0ee2
Andrew Gilbert <andrew.gilbert@cern.ch>
2015-10-05 14:38:53 +0200

    Adding code and instructions to build a RooMorphingPdf of the charged Higgs to tau+nu datacards and reproduce model-independent limits

--- CombinePdfs/python/MSSM.py
+++ CombinePdfs/python/MSSM.py
@@ -126,6 +126,35 @@ class MSSMHiggsModel(PhysicsModel):
         else:
             return 1
 
+class BRChargedHiggs(PhysicsModel):
+    def __init__(self):
+        PhysicsModel.__init__(self)
+
+    def doParametersOfInterest(self):
+        """Create POI and other parameters, and define the POI set."""
+        self.modelBuilder.doVar('BR[0,0,1]');
+
+        self.modelBuilder.doSet('POI','BR')
+
+        self.modelBuilder.factory_('expr::Scaling_HH("@0*@0", BR)')
+        self.modelBuilder.factory_('expr::Scaling_WH("2 * (1-@0)*@0", BR)')
+        #self.modelBuilder.factory_('expr::Scaling_tt("(1-@0)*(1-@0)", BR)')
+        self.modelBuilder.factory_('expr::Scaling_tt("1 - (@0+@1)", Scaling_HH, Scaling_WH)')
+
+        self.processScaling = { 'tt_ttHchHch':'HH', 'tt_ttHchW':'WH', 'tt':'tt' }
+
+        # self.modelBuilder.out.Print()
+        
+    def getYieldScale(self,bin,process):
+        for prefix, model in self.processScaling.iteritems():
+            if process == prefix:
+                print 'Scaling %s/%s as %s' % (bin, process, 'Scaling_'+model)
+                return 'Scaling_'+model
+        return 1
+
+
+
 
 MSSM = MSSMHiggsModel()
+brChargedHiggs = BRChargedHiggs()
 
