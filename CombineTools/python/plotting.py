ea3378d680e21d779dd7ab84cc6c763536e75556
Adinda de Wit <adinda.de-wit12@imperial.ac.uk>
2015-12-09 09:04:33 +0000

    Adding limit comparison script

--- CombineTools/python/plotting.py
+++ CombineTools/python/plotting.py
@@ -935,6 +935,18 @@ def LimitBandTGraphFromJSON(js, central, lo, hi):
     graph.Sort()
     return graph
 
+def GraphDifference(graph1,graph2):
+    xvals =[]
+    yvals =[]
+    if graph1.GetN() != graph2.GetN():
+        return graph1
+    for i in range(graph1.GetN()):
+        xvals.append(graph1.GetX()[i])
+        yvals.append(2*abs(graph1.GetY()[i]-graph2.GetY()[i])/(graph1.GetY()[i]+graph2.GetY()[i]))
+    diff_graph = R.TGraph(len(xvals),array('d',xvals),array('d',yvals))
+    diff_graph.Sort()
+    return diff_graph 
+
 def Set(obj, **kwargs):
   for key, value in kwargs.iteritems():
     getattr(obj, 'Set'+key)(value)
