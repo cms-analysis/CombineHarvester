998ec2f3c67b2bf810c9c9494ca2113214d965f9
Christian Veelken <christian.veelken@cern.ch>
2016-07-04 15:47:43 +0300

    - added "2lss_1tau" to name of all code specific to 2lss_1tau channel - added code for 1l_2tau channel

---
+++ ttH_htt/make_1l_2tau.sh
@@ -0,0 +1,13 @@
+#! /bin/csh -f
+
+WriteDatacards_1l_2tau
+
+combine -M MaxLikelihoodFit -m 125 ttH_1l_2tau.txt
+
+combine -M Asymptotic -m 125 ttH_1l_2tau.txt
+
+PostFitShapes -d ttH_1l_2tau.txt -o ttH_1l_2tau_shapes.root -m 125 -f mlfit.root:fit_s --postfit --sampling --print
+
+cd macros
+root -b -n -q -l makePostFitPlots_1l_2tau.C++
+cd -
