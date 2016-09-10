b21a6a3c254a591f05d22a732555db7e831d18fd
Andrew Gilbert <andrew.gilbert@cern.ch>
2016-09-10 17:24:11 +0200

    More realistic uncertainty model for the tau id tag and probe, and add script to produce input for workspace

--- MSSM2016/scripts/setup-TauTP.py
+++ MSSM2016/scripts/setup-TauTP.py
@@ -16,7 +16,7 @@ procs = {
   'bkg': ['ZL', 'ZJ', 'W', 'QCD', 'TT', 'VV']
 }
 
-cats = [(0, '%s_fail' % args.dir), (1, '%s_pass' % args.dir)]
+cats = [(0, 'fail'), (1, 'pass')]
 
 cb.AddObservations(['*'], ['tauTP'], ["13TeV"], ['mt'], cats)
 cb.AddProcesses(['*'], ['tauTP'], ["13TeV"], ['mt'], procs['bkg'], cats, False)
@@ -25,50 +25,38 @@ cb.AddProcesses(['*'], ['tauTP'], ["13TeV"], ['mt'], procs['sig'], cats, True)
 print '>> Adding systematic uncertainties...'
 # cb.SetFlag('filters-use-regex', True)
 cb.cp().AddSyst(
-    cb, 'CMS_scale_t_$PROCESS_$BINID', 'shape', ch.SystMap()(1.00))
+    cb, 'CMS_scale_t_$PROCESS_$BIN', 'shape', ch.SystMap()(1.00))
 cb.syst_name(['blah'])
 
-cb.cp().backgrounds().AddSyst(
-    cb, 'norm_$PROCESS_$BINID', 'lnN', ch.SystMap()(1.10))
+cb.cp().process(['QCD', 'TT', 'VV']).AddSyst(
+    cb, 'norm_$PROCESS_$BIN', 'lnN', ch.SystMap()(1.10))
 
-cb.cp().process(['ZTT']).AddSyst(
-    cb, 'norm_$PROCESS', 'lnN', ch.SystMap()(1.05))
+cb.cp().process(['ZL', 'ZJ']).AddSyst(
+    cb, 'norm_$PROCESS_$BIN', 'lnN', ch.SystMap()(1.20))
+
+cb.cp().process(['ZTT', 'ZL', 'ZJ', 'W', 'TT', 'VV']).AddSyst(
+    cb, 'CMS_eff_m', 'lnN', ch.SystMap()(1.02))
+
+cb.cp().process(['ZTT', 'ZL', 'ZJ', 'W', 'TT', 'VV']).AddSyst(
+    cb, 'lumi_13TeV_2016', 'lnN', ch.SystMap()(1.062))
+
+cb.cp().process(['ZTT', 'ZL', 'ZJ']).AddSyst(
+    cb, 'CMS_htt_zjXsec_13TeV', 'lnN', ch.SystMap()(1.05))
+
+cb.cp().process(['TT']).AddSyst(
+    cb, 'CMS_htt_tjXsec_13TeV', 'lnN', ch.SystMap()(1.06))
+
+cb.cp().process(['W']).AddSyst(
+    cb, 'CMS_htt_wjXsec_13TeV', 'lnN', ch.SystMap()(1.10))
 
 cb.cp().AddSyst(
     cb, 'CMS_scale_t', 'shape', ch.SystMap()(1.00))
 
 print '>> Extracting histograms from input root files...'
 cb.cp().ExtractShapes(
-    args.input, 'analysis/$BIN/$PROCESS', 'analysis/$BIN/$PROCESS_$SYSTEMATIC')
+    args.input, 'analysis/%s_$BIN/$PROCESS' % args.dir, 'analysis/%s_$BIN/$PROCESS_$SYSTEMATIC' % args.dir)
 
-cb.cp().syst_name(['CMS_scale_t']).ForEachSyst(lambda s: s.set_name('CMS_scale_t_%s_%i' % (s.process(), s.bin_id())))
-
-# Now we'll do a trick and scale the ZTT to the pass+fail rate in both categories
-yield_fail = cb.cp().bin_id([0]).process(['ZTT']).GetRate()
-yield_pass = cb.cp().bin_id([1]).process(['ZTT']).GetRate()
-
-yield_W = cb.cp().bin_id([1]).process(['W']).GetRate()
-yield_QCD = cb.cp().bin_id([1]).process(['QCD']).GetRate()
-
-eff_initial = yield_pass / (yield_pass + yield_fail)
-
-cb.cp().process(['ZTT']).ForEachProc(lambda p: p.set_rate(yield_pass + yield_fail))
-
-print 'ZTT: %g %g %g' % (yield_pass, yield_fail, eff_initial)
-print 'W/ZTT: %g %g' % (yield_W, (yield_W/yield_pass))
-print 'QCD/ZTT: %g %g' % (yield_QCD, (yield_QCD/yield_pass))
-
-cb.cp().process(['ZTT']).bin_id([1]).AddSyst(
-    cb, 'effsf', 'rateParam', ch.SystMap()(eff_initial))
-
-cb.GetParameter('effsf').set_val(1.0)
-cb.GetParameter('effsf').set_range(0.5, 1.5)
-
-cb.cp().process(['ZTT']).bin_id([1]).AddSyst(
-    cb, 'pass', 'rateParam', ch.SystMap()(('(%g)' % eff_initial, 'effsf')))
-
-cb.cp().process(['ZTT']).bin_id([0]).AddSyst(
-    cb, 'fail', 'rateParam', ch.SystMap()(('(1-@0*%g)' % eff_initial, 'effsf')))
+cb.cp().syst_name(['CMS_scale_t']).ForEachSyst(lambda s: s.set_name('CMS_scale_t_%s_%s' % (s.process(), s.bin())))
 
 
 print '>> Generating bbb uncertainties...'
@@ -77,6 +65,55 @@ bbb.SetAddThreshold(0.0)
 bbb.SetMergeThreshold(0.2)
 bbb.SetFixNorm(False)
 bbb.MergeAndAdd(cb, cb)
+
+
+# Now we'll do a trick and scale the ZTT to the pass+fail rate in both categories
+yield_fail = cb.cp().bin_id([0]).process(['ZTT']).GetRate()
+yield_pass = cb.cp().bin_id([1]).process(['ZTT']).GetRate()
+
+yield_W_fail = cb.cp().bin_id([0]).process(['W']).GetRate()
+yield_W_pass = cb.cp().bin_id([1]).process(['W']).GetRate()
+
+yield_QCD = cb.cp().bin_id([1]).process(['QCD']).GetRate()
+
+eff_initial = yield_pass / (yield_pass + yield_fail)
+eff_W_initial = yield_W_pass / (yield_W_pass + yield_W_fail)
+
+cb.cp().process(['ZTT']).ForEachProc(lambda p: p.set_rate(yield_pass + yield_fail))
+cb.cp().process(['W']).ForEachProc(lambda p: p.set_rate(yield_W_pass + yield_W_fail))
+
+print 'ZTT: %g %g %g' % (yield_pass, yield_fail, eff_initial)
+print 'W/ZTT: %g %g' % (yield_W_pass, (yield_W_pass/yield_pass))
+print 'QCD/ZTT: %g %g' % (yield_QCD, (yield_QCD/yield_pass))
+
+cb.cp().process(['ZTT']).bin_id([1]).AddSyst(
+    cb, 'effsf', 'rateParam', ch.SystMap()(1.0))
+
+cb.cp().process(['W']).bin_id([1]).AddSyst(
+    cb, 'fakesf', 'rateParam', ch.SystMap()(1.0))
+
+cb.cp().process(['ZTT', 'ZL', 'ZJ']).AddSyst(
+    cb, 'DY_XS', 'rateParam', ch.SystMap()(1.0))
+
+cb.GetParameter('effsf').set_val(1.0)
+cb.GetParameter('effsf').set_range(0.5, 1.5)
+
+cb.GetParameter('fakesf').set_val(1.0)
+cb.GetParameter('fakesf').set_range(0.5, 2.5)
+
+cb.cp().process(['ZTT']).bin_id([1]).AddSyst(
+    cb, 'pass', 'rateParam', ch.SystMap()(('(%g)' % eff_initial, 'effsf')))
+
+cb.cp().process(['ZTT']).bin_id([0]).AddSyst(
+    cb, 'fail', 'rateParam', ch.SystMap()(('(1-@0*%g)' % eff_initial, 'effsf')))
+
+cb.cp().process(['W']).bin_id([1]).AddSyst(
+    cb, 'passW', 'rateParam', ch.SystMap()(('(%g)' % eff_W_initial, 'fakesf')))
+
+cb.cp().process(['W']).bin_id([0]).AddSyst(
+    cb, 'failW', 'rateParam', ch.SystMap()(('(1-@0*%g)' % eff_W_initial, 'fakesf')))
+
+
 
 
 # print '>> Setting standardised bin names...'
