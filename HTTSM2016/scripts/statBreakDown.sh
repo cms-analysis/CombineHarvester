12c30702aa6a14c158b7a4c1da3fd92146170481
Tyler Ruggles <truggles@wisc.edu>
2016-12-14 00:22:43 +0100

    for studying the breakdown of nuisance parameter groups on total uncertainty

---
+++ HTTSM2016/scripts/statBreakDown.sh
@@ -0,0 +1,34 @@
+# Based on Combine Tutorial "Uncertainty breakdown and nuisance parameter groups"
+
+# This needs to have:
+#    texName.json 
+#    plot1DScan.py
+#    in the same directory
+
+
+# From Combine Tutorial
+# First perform the fit and save a copy of the workspace in the output file with a "snapshot" of the best- fit model parameters included
+combine -M MultiDimFit -m 125 --algo grid --points 20 --rMin 0.001 --rMax 3 cmb/125/workspace.root -n nominal -t -1 --expectSignal=1
+combine -M MultiDimFit --algo none --rMin 0.001 --rMax 3 cmb/125/workspace.root -m 125 -t -1 --expectSignal=1 -n bestfit --saveWorkspace
+
+# Can then repeat the scan by first loading this snapshot, then freezing all the nuisance parameters. The uncertainty from this scan gives us the statistical component of the uncertainty. By defining: sigma_total^2 = sigma_stat^2 + sigma_syst^2 we can infer the systematic component.
+combine -M MultiDimFit --algo grid --points 20 --rMin 0.001 --rMax 3 -m 125 -t -1 --expectSignal=1 -n stat \
+higgsCombinebestfit.MultiDimFit.mH125.root --snapshotName MultiDimFit --freezeNuisances all
+
+plot1DScan.py higgsCombinenominal.MultiDimFit.mH125.root \
+--POI r -o cms_output \
+--others 'higgsCombinestat.MultiDimFit.mH125.root:Freeze all:2' \
+--breakdown syst,stat
+
+combine -M MultiDimFit --algo grid --points 20 --rMin 0.001 --rMax 3 -m 125 -t -1 --expectSignal=1 -n theory \
+higgsCombinebestfit.MultiDimFit.mH125.root --snapshotName MultiDimFit --freezeNuisanceGroups theory
+
+
+python ./plot1DScan.py --main higgsCombinenominal.MultiDimFit.mH125.root \
+--POI r -o cms_output \
+--others \
+    'higgsCombinetheory.MultiDimFit.mH125.root:Freeze theory:2' \
+    'higgsCombinestat.MultiDimFit.mH125.root:Freeze all:3' \
+--breakdown theory,syst,stat
+
+#N.B. breakdown works by taking the "observed" uncertainty and subtracting the first listed uncertainty fit, then the second ... to leave you with a final estimate of the statistical uncertainty
