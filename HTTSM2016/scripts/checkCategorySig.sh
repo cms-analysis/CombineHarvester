14e3b0a411e64f86e98e312becc9cfc143acf2a4
Tyler Ruggles <truggles@wisc.edu>
2016-12-14 01:05:04 +0100

    categories comp includes CMB

--- HTTSM2016/scripts/checkCategorySig.sh
+++ HTTSM2016/scripts/checkCategorySig.sh
@@ -1,9 +1,9 @@
 
-
 combineCards.py htt_*_1_13TeV/125/htt_*_1_13TeV.txt > zeroJetDC.txt
 combineCards.py htt_*_2_13TeV/125/htt_*_2_13TeV.txt htt_ttbar_1_13TeV/125/*.txt > boostedDC.txt
 combineCards.py htt_*_3_13TeV/125/htt_*_3_13TeV.txt htt_ttbar_1_13TeV/125/*.txt > vbfDC.txt
 
+combineTool.py -M T2W -i {cmb,em,et,mt,tt}/* -o workspace.root --parallel 8
 combineTool.py -M T2W -m 125 -i zeroJetDC.txt -o workspace1.root --parallel 8
 combineTool.py -M T2W -m 125 -i boostedDC.txt -o workspace2.root --parallel 8
 combineTool.py -M T2W -m 125 -i vbfDC.txt -o workspace3.root --parallel 8
@@ -13,3 +13,8 @@ for NUM in 1 2 3; do
     echo "Number:" ${NUM}
     combine -M ProfileLikelihood --significance workspace${NUM}.root -t -1 --expectSignal=1
 done
+
+echo "Combined"
+combine -M ProfileLikelihood --significance cmb/125/workspace.root -t -1 --expectSignal=1
+
+
