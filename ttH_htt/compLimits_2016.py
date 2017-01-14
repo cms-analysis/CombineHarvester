8c0a0bfec130cc76668a6628ffa6fc162a238679
Christian Veelken <christian.veelken@cern.ch>
2017-01-14 17:58:06 +0200

    - updated file paths to Tallinn datacards - updated macros for making postfit plot

--- ttH_htt/compLimits_2016.py
+++ ttH_htt/compLimits_2016.py
@@ -1,49 +1,58 @@
 import os, subprocess, sys
 
 channels = [
-  "1l_2tau_3jets",
-  "1l_2tau_4jets",
-  #"2lss_1tau_3L",
-  #"2lss_1tau_2lepton",
-  #"2lss_1tau_3L_3jets",
-  #"2lss_1tau_3L_4jets",
-  #"3l_1tau"
+  ##"1l_2tau_LLR",
+  ##"1l_2tau_ND",
+  "1l_2tau_Tallinn",
+  ##"2lss_1tau_Cornell",
+  ##"2lss_1tau_LLR",
+  "2lss_1tau_Tallinn",
+  ##"3l_1tau_LLR",
+  "3l_1tau_Tallinn",
 ]
 
 shapeVariables = {
-  "1l_2tau_3jets"   : [ "EventCounter", "numJets", "mvaOutput_1l_2tau_ttbar_TMVA", "mvaOutput_1l_2tau_ttbar_sklearn", "mTauTauVis" ],
-  "1l_2tau_4jets"   : [ "EventCounter", "numJets", "mvaOutput_1l_2tau_ttbar_TMVA", "mvaOutput_1l_2tau_ttbar_sklearn", "mTauTauVis" ],
-  "2lss_1tau_3L" : [ "EventCounter", "numJets", "mvaDiscr_2lss", "mvaOutput_2lss_1tau_ttbar_TMVA", "mvaOutput_2lss_1tau_ttbar_sklearn", "mTauTauVis", "memOutput_LR" ],
-  "2lss_1tau_2lepton" : [ "EventCounter", "numJets", "mvaDiscr_2lss", "mvaOutput_2lss_1tau_ttbar_TMVA", "mvaOutput_2lss_1tau_ttbar_sklearn", "mTauTauVis" ],
-  "2lss_1tau_3L_3jets" : [ "EventCounter", "numJets", "mvaDiscr_2lss", "mvaOutput_2lss_1tau_ttbar_TMVA", "mvaOutput_2lss_1tau_ttbar_sklearn", "mTauTauVis", "memOutput_LR" ],
-  "2lss_1tau_3L_4jets" : [ "EventCounter", "numJets", "mvaDiscr_2lss", "mvaOutput_2lss_1tau_ttbar_TMVA", "mvaOutput_2lss_1tau_ttbar_sklearn", "mTauTauVis", "memOutput_LR" ],
-  "3l_1tau"   : [ "EventCounter", "numJets", "mvaDiscr_3l", "mTauTauVis" ]
+  "1l_2tau_LLR"       : [ "mTauTauVis" ],
+  "1l_2tau_ND"        : [ "mTauTauVis" ],
+  "1l_2tau_Tallinn"   : [ "mTauTauVis" ],
+  "2lss_1tau_Cornell" : [ "mvaDiscr_2lss" ],
+  "2lss_1tau_LLR"     : [ "mvaDiscr_2lss" ],
+  "2lss_1tau_Tallinn" : [ "mvaDiscr_2lss" ],
+  "3l_1tau_LLR"       : [ "mvaDiscr_3l" ],
+  "3l_1tau_Tallinn"   : [ "mvaDiscr_3l" ],
 }
 
 datacardFiles = {
-  "1l_2tau_3jets" : "ttH_1l_2tau_2016Nov09_3jets_dR03mvaTight_%s.input.root",
-  "1l_2tau_4jets" : "ttH_1l_2tau_2016Nov09_4jets_dR03mvaTight_%s.input.root",
-  "2lss_1tau_3L" : "ttH_2lss_1tau_2016Nov03_dR03mvaMedium_%s.input.root",
-  "2lss_1tau_2lepton" : "ttH_2lss_1tau_2016Oct19_2lepton_dR03mvaMedium_%s.input.root",
-  "2lss_1tau_3L_3jets" : "ttH_2lss_1tau_2016Nov09_3jets_dR03mvaMedium_%s.input.root",
-  "2lss_1tau_3L_4jets" : "ttH_2lss_1tau_2016Nov09_4jets_dR03mvaMedium_%s.input.root",
-  "3l_1tau" : "ttH_3l_1tau_2016Oct19_dR03mvaMedium_%s.input.root"
+  "1l_2tau_LLR"       : "LLR/datacard_mvis_1l2tau_syst_12.9fb_2017Jan13.root",
+  "1l_2tau_ND"        : "ND/sync_workspace.root",
+  "1l_2tau_Tallinn"   : "Tallinn/datacard_mvis_1l2tau_12.9fb_2017Jan13.root",
+  "2lss_1tau_Cornell" : "Cornell/ttH_2lss_1tau.input.root",
+  "2lss_1tau_LLR"     : "LLR/datacard_MVA_2lSS_1tau_nofaketau_syst_12.9fb_2017Jan13.root",
+  "2lss_1tau_Tallinn" : "Tallinn/datacard_MVA_2lSS_1tau_nofaketau_12.9fb_2017Jan13.root",
+  "3l_1tau_LLR"       : "LLR/datacard_MVA_3l_1tau_12.9fb_2017Jan13.root",
+  "3l_1tau_Tallinn"   : "Tallinn/datacard_MVA_3l_1tau_nofaketau_12.9fb_2017Jan13.root",
 }
 
 WriteDatacard_executables = {
-  "1l_2tau_3jets"   : 'WriteDatacards_1l_2tau',
-  "1l_2tau_4jets"   : 'WriteDatacards_1l_2tau',
-  "2lss_1tau_3L_3jets" : 'WriteDatacards_2lss_1tau',
-  "2lss_1tau_3L_4jets" : 'WriteDatacards_2lss_1tau',
-  "2lss_1tau_2lepton" : 'WriteDatacards_2lss_1tau',
-  "3l_1tau"   : 'WriteDatacards_3l_1tau'
+  "1l_2tau_LLR"       : 'WriteDatacards_1l_2tau',
+  "1l_2tau_ND"        : 'WriteDatacards_1l_2tau',
+  "1l_2tau_Tallinn"   : 'WriteDatacards_1l_2tau',
+  "2lss_1tau_Cornell" : 'WriteDatacards_2lss_1tau',
+  "2lss_1tau_LLR"     : 'WriteDatacards_2lss_1tau',
+  "2lss_1tau_Tallinn" : 'WriteDatacards_2lss_1tau',
+  "3l_1tau_LLR"       : 'WriteDatacards_3l_1tau',
+  "3l_1tau_Tallinn"   : 'WriteDatacards_3l_1tau',
 }
 
 makePostFitPlots_macros = {
-  "1l_2tau"   : 'makePostFitPlots_1l_2tau.C',
-  "2lss_1tau_3L" : 'makePostFitPlots_2lss_1tau.C',
-  "2lss_1tau_2lepton" : 'makePostFitPlots_2lss_1tau.C',
-  "3l_1tau"   : 'makePostFitPlots_3l_1tau.C'
+  "1l_2tau_LLR"       : 'makePostFitPlots_1l_2tau.C',
+  "1l_2tau_ND"        : 'makePostFitPlots_1l_2tau.C',
+  "1l_2tau_Tallinn"   : 'makePostFitPlots_1l_2tau.C',
+  "2lss_1tau_Cornell" : 'makePostFitPlots_2lss_1tau.C',
+  "2lss_1tau_LLR"     : 'makePostFitPlots_2lss_1tau.C',
+  "2lss_1tau_Tallinn" : 'makePostFitPlots_2lss_1tau.C',
+  "3l_1tau_LLR"       : 'makePostFitPlots_3l_1tau.C',
+  "3l_1tau_Tallinn"   : 'makePostFitPlots_3l_1tau.C',
 }    
 
 def run_cmd(command):
@@ -53,7 +62,7 @@ def run_cmd(command):
   return stdout
 
 workingDir = os.getcwd()
-datacardDir = "/home/veelken/public/HIG16022_datacards/Tallinn/cut_optimization/"
+datacardDir = "/home/veelken/public/HIG16022_datacards/"
 
 for channel in channels:
   for shapeVariable in shapeVariables[channel]:
@@ -61,16 +70,22 @@ for channel in channels:
     for search_string in [ "1l_2tau", "2lss_1tau", "3l_1tau" ]:
       if channel.find(search_string) != -1:
         channel_base = search_string
-    datacardFile_input = os.path.join(datacardDir, channel_base, datacardFiles[channel] % shapeVariable)
-    datacardFile_output = "ttH_%s.root" % channel
+    ##datacardFile_input = os.path.join(datacardDir, channel_base, datacardFiles[channel] % shapeVariable)
+    datacardFile_input = os.path.join(datacardDir, datacardFiles[channel])
+    datacardFile_output = os.path.join(workingDir, "limits", "ttH_%s.root" % channel)
+    run_cmd('rm ttH_%s.txt' % channel)
+    run_cmd('rm %s' % datacardFile_output)
     run_cmd('%s --input_file=%s --output_file=%s --add_shape_sys=false' % (WriteDatacard_executables[channel], datacardFile_input, datacardFile_output))
-    txtFile = os.path.join(workingDir, "cut_optimization", "ttH_%s_%s_2016.txt" % (channel_base, shapeVariable))
-    run_cmd('mv ttH_%s.txt %s' % (channel, txtFile))
-    run_cmd('combine -M MaxLikelihoodFit -t -1 -m 125 %s' % txtFile)
-    logFile = os.path.join(workingDir, "cut_optimization", "ttH_%s_%s_2016.log" % (channel, shapeVariable))
+    txtFile = datacardFile_output.replace(".root", ".txt")
+    run_cmd('cp %s %s' % (datacardFile_output, datacardFile_output.replace("/limits/", "/")))
+    ##run_cmd('combine -M MaxLikelihoodFit -t -1 -m 125 %s' % txtFile)
+    run_cmd('combine -M MaxLikelihoodFit -m 125 %s' % txtFile)
+    logFile = os.path.join(workingDir, "limits", "ttH_%s_%s_2016.log" % (channel, shapeVariable))
     run_cmd('rm %s' % logFile)
-    run_cmd('combine -M Asymptotic -t -1 -m 125 %s &> %s' % (txtFile, logFile))
-    rootFile = os.path.join(workingDir, "cut_optimization", "ttH_%s_%s_2016_shapes.root" % (channel_base, shapeVariable))
+    ##run_cmd('combine -M Asymptotic -t -1 -m 125 %s &> %s' % (txtFile, logFile))
+    run_cmd('combine -M Asymptotic -m 125 %s &> %s' % (txtFile, logFile))
+    rootFile = os.path.join(workingDir, "limits", "ttH_%s_%s_2016_shapes.root" % (channel_base, shapeVariable))
+    run_cmd('rm %s' % rootFile)
     run_cmd('PostFitShapes -d %s -o %s -m 125 -f mlfit.root:fit_s --postfit --sampling --print' % (txtFile, rootFile))
     ##run_cmd('root -b -n -q -l %s++' % os.path.join(workingDir, "macros", makePostFitPlots_macros[channel]))
 
