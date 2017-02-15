1005934aeff4643d6ef3e7a601b1ae3fa31459ab
thomas strebler <thomas.strebler@llr.in2p3.fr>
2017-02-15 18:39:49 +0100

    Updated WriteDatacards_2lss_1tau

--- ttH_htt/bin/WriteDatacards_2lss_1tau.cpp
+++ ttH_htt/bin/WriteDatacards_2lss_1tau.cpp
@@ -20,7 +20,7 @@ namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
 
-  std::string input_file, output_file;
+  std::string input_file, output_file, bin_name;
   double lumi = -1.;
   bool add_shape_sys = true;
   po::variables_map vm;
@@ -29,7 +29,8 @@ int main(int argc, char** argv) {
     ("input_file,i", po::value<string>(&input_file)->default_value("Tallinn/ttH_2lss_1tau_2016Jul11_Tight.input.root"))
     ("output_file,o", po::value<string>(&output_file)->default_value("ttH_2lss_1tau.root"))
     ("lumi,l", po::value<double>(&lumi)->default_value(lumi))
-    ("add_shape_sys,s", po::value<bool>(&add_shape_sys)->default_value(true));
+    ("add_shape_sys,s", po::value<bool>(&add_shape_sys)->default_value(true))
+    ("bin_name,b", po::value<string>(&bin_name)->default_value("ttH_2lss_1tau"));
   po::store(po::command_line_parser(argc, argv).options(config).run(), vm);
   po::notify(vm);
 
@@ -50,7 +51,8 @@ int main(int argc, char** argv) {
   // Here we will just define two categories for an 8TeV analysis. Each entry in
   // the vector below specifies a bin name and corresponding bin_id.
   ch::Categories cats = {
-      {1, "ttH_2lss_1tau"}
+    //{1, "ttH_2lss_1tau"}
+    {1, bin_name}
     };
   // ch::Categories is just a typedef of vector<pair<int, string>>
   //! [part1]
@@ -89,7 +91,8 @@ int main(int argc, char** argv) {
   //! [part5]
   cb.cp().signals()
       .AddSyst(cb, "lumi_$ERA", "lnN", SystMap<era>::init
-      ({"13TeV"}, 1.027));
+	       //({"13TeV"}, 1.027));
+	       ({"13TeV_2016"}, 1.06));
   //! [part5]
 
   //! [part6]
@@ -97,28 +100,34 @@ int main(int argc, char** argv) {
       .AddSyst(cb, "pdf_Higgs", "lnN", SystMap<>::init(1.036));
   cb.cp().process(sig_procs)
     .AddSyst(cb, "QCDscale_ttH", "lnN", SystMapAsymm<>::init(0.915, 1.058));
-  cb.cp().process(sig_procs)
+  if ( add_shape_sys ) {
+    cb.cp().process(sig_procs)
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttH_x1", "shape", SystMap<>::init(1.0));
-  cb.cp().process(sig_procs)
+    cb.cp().process(sig_procs)
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttH_y1", "shape", SystMap<>::init(1.0));
+  }
 
   cb.cp().process({"TTW"})
       .AddSyst(cb, "pdf_qqbar", "lnN", SystMap<>::init(1.04));
   cb.cp().process({"TTW"})
       .AddSyst(cb, "QCDscale_ttW", "lnN", SystMap<>::init(1.12));
-  cb.cp().process({"TTW"})
+  if ( add_shape_sys ) {
+    cb.cp().process({"TTW"})
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttW_x1", "shape", SystMap<>::init(1.0));
-  cb.cp().process({"TTW"})
+    cb.cp().process({"TTW"})
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttW_y1", "shape", SystMap<>::init(1.0));
+  }
 
   cb.cp().process({"TTZ"})
       .AddSyst(cb, "pdf_gg", "lnN", SystMap<>::init(0.966));
   cb.cp().process({"TTZ"})
       .AddSyst(cb, "QCDscale_ttZ", "lnN", SystMap<>::init(1.11));
-  cb.cp().process({"TTZ"})
+  if ( add_shape_sys ) {
+    cb.cp().process({"TTZ"})
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttZ_x1", "shape", SystMap<>::init(1.0));
-  cb.cp().process({"TTZ"})
+    cb.cp().process({"TTZ"})
       .AddSyst(cb, "CMS_ttHl_thu_shape_ttZ_y1", "shape", SystMap<>::init(1.0));
+  }
 
   //cb.cp().process({"WZ"})
   //    .AddSyst(cb, "CMS_ttHl_WZ_4j", "lnN", SystMap<>::init(2.0));
@@ -131,21 +140,27 @@ int main(int argc, char** argv) {
       .AddSyst(cb, "CMS_ttHl_Rares", "lnN", SystMap<>::init(1.5));
 
   cb.cp().process({"fakes_data"})
-      .AddSyst(cb, "CMS_ttHl_FRe_norm", "lnN", SystMap<>::init(1.2));
+      .AddSyst(cb, "CMS_ttHl_Clos_m_norm", "lnN", SystMap<>::init(1.2));
+  cb.cp().process({"fakes_data"})
+      .AddSyst(cb, "CMS_ttHl_Clos_e_norm", "lnN", SystMap<>::init(0.95));
+
+  cb.cp().process({"fakes_data"})
+    .AddSyst(cb, "CMS_ttHl_FRm_norm", "lnN", SystMap<>::init(1.3));
+  cb.cp().process({"fakes_data"})
+    .AddSyst(cb, "CMS_ttHl_FRe_norm", "lnN", SystMap<>::init(1.25));
+
+
   //cb.cp().process({"fakes_data"})
   //    .AddSyst(cb, "CMS_ttHl_FRe_shape_2lss_corr1", "shape", SystMap<>::init(1.0));
   //cb.cp().process({"fakes_data"})
   //    .AddSyst(cb, "CMS_ttHl_FRe_shape_2lss_anticorr1", "shape", SystMap<>::init(1.0));  
-  cb.cp().process({"fakes_data"})
-      .AddSyst(cb, "CMS_ttHl_Clos_e_norm", "lnN", SystMap<>::init(0.96));
-  cb.cp().process({"fakes_data"})
-      .AddSyst(cb, "CMS_ttHl_FRm_norm", "lnN", SystMap<>::init(1.2));
+
+
   //cb.cp().process({"fakes_data"})
   //    .AddSyst(cb, "CMS_ttHl_FRm_shape_2lss_corr1", "shape", SystMap<>::init(1.0));
   //cb.cp().process({"fakes_data"})
   //    .AddSyst(cb, "CMS_ttHl_FRm_shape_2lss_anticorr1", "shape", SystMap<>::init(1.0)); 
-  cb.cp().process({"fakes_data"})
-      .AddSyst(cb, "CMS_ttHl_Clos_m_norm", "lnN", SystMap<>::init(1.04));
+
 
   cb.cp().process({"flips_data"})
       .AddSyst(cb, "CMS_ttHl_QF", "lnN", SystMap<>::init(1.3));
@@ -159,7 +174,7 @@ int main(int argc, char** argv) {
   cb.cp().process(ch::JoinStr({sig_procs, {"TTW", "TTZ", "Rares"}}))
       .AddSyst(cb, "CMS_ttHl_lepEff_tight", "lnN", SystMap<>::init(1.06));
   cb.cp().process(ch::JoinStr({sig_procs, {"TTW", "TTZ", "Rares"}}))
-      .AddSyst(cb, "CMS_ttHl_tauID", "lnN", SystMap<>::init(1.06));
+      .AddSyst(cb, "CMS_ttHl_tauID", "lnN", SystMap<>::init(1.1));
   if ( add_shape_sys ) {
     cb.cp().process(ch::JoinStr({sig_procs, {"TTW", "TTZ", "Rares"}}))
         .AddSyst(cb, "CMS_ttHl_JES", "shape", SystMap<>::init(1.0));
@@ -169,8 +184,8 @@ int main(int argc, char** argv) {
         .AddSyst(cb, "CMS_ttHl_tauES", "shape", SystMap<>::init(1.0));
   }
 
-  cb.cp().process(ch::JoinStr({sig_procs, {"TTW", "TTZ"}}))
-      .AddSyst(cb, "CMS_eff_m", "lnN", SystMap<>::init(1.02));
+  //cb.cp().process(ch::JoinStr({sig_procs, {"TTW", "TTZ"}}))
+  //    .AddSyst(cb, "CMS_eff_m", "lnN", SystMap<>::init(1.02));
 
   if ( add_shape_sys ) {
     for ( auto s : {"HF", "HFStats1", "HFStats2", "LF", "LFStats1", "LFStats2", "cErr1", "cErr2"} ) {
@@ -239,3 +254,4 @@ int main(int argc, char** argv) {
   //! [part9]
 
 }
+
