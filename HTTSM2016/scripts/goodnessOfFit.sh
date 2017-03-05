eb053e54c4a829b237eccce8c1b81fb97bd50d58
Tyler Ruggles <truggles@wisc.edu>
2017-03-05 21:04:20 +0100

    Set to 500 toys each

--- HTTSM2016/scripts/goodnessOfFit.sh
+++ HTTSM2016/scripts/goodnessOfFit.sh
@@ -30,7 +30,7 @@ combineTool.py -M T2W -i {cmb,em,et,mt,tt}/* -o workspace.root --parallel 12
 
 # Do Anderson Darling first
 ALGO=AD
-combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 5 -s 0:10:1 --parallel 12
+combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 25 -s 0:19:1 --parallel 12
 combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO" --fixedSignalStrength=1
 
 combineTool.py -M CollectGoodnessOfFit --input cmb/125/higgsCombine.${ALGO}.GoodnessOfFit.mH125.root cmb/125/higgsCombine.${ALGO}.toys.GoodnessOfFit.mH125.*.root -o collectGoodness_${ALGO}.json
@@ -43,7 +43,7 @@ python ../../../CombineTools/scripts/plotGof.py --statistic ${ALGO} --mass 125.0
 
 # Do KS test
 ALGO=KS
-combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 5 -s 0:10:1 --parallel 12
+combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 25 -s 0:19:1 --parallel 12
 combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d cmb/125/workspace.root -n ".$ALGO" --fixedSignalStrength=1
 
 combineTool.py -M CollectGoodnessOfFit --input cmb/125/higgsCombine.${ALGO}.GoodnessOfFit.mH125.root cmb/125/higgsCombine.${ALGO}.toys.GoodnessOfFit.mH125.*.root -o collectGoodness_${ALGO}.json
@@ -63,7 +63,7 @@ for CHANNEL in em et mt tt; do
     for BIN in 1 2 3; do
         echo "Saturated for ${CHANNEL} ${BIN}"
         combineTool.py -M T2W -i htt_${CHANNEL}_${BIN}_13TeV/125/*.txt -o workspace_${CHANNEL}_${BIN}.root --parallel 2
-        combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d htt_${CHANNEL}_${BIN}_13TeV/125/workspace_${CHANNEL}_${BIN}.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 5 -s 0:10:1 --parallel 12
+        combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d htt_${CHANNEL}_${BIN}_13TeV/125/workspace_${CHANNEL}_${BIN}.root -n ".$ALGO.toys" --fixedSignalStrength=1 -t 25 -s 0:19:1 --parallel 12
         combineTool.py -M GoodnessOfFit --algorithm $ALGO -m 125 --there -d htt_${CHANNEL}_${BIN}_13TeV/125/workspace_${CHANNEL}_${BIN}.root -n ".$ALGO" --fixedSignalStrength=1
         combineTool.py -M CollectGoodnessOfFit --input htt_${CHANNEL}_${BIN}_13TeV/125/higgsCombine.saturated.GoodnessOfFit.mH125.root htt_${CHANNEL}_${BIN}_13TeV/125/higgsCombine.saturated.toys.GoodnessOfFit.mH125.*.root -o htt_${CHANNEL}_${BIN}_saturated.json
     done
