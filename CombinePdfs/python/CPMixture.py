ad5a67ee7679c57582acc99887353e79da6883b7
danielwinterbottom <daniel.winterbottom@cern.ch>
2018-01-09 21:55:38 +0000

    removing xs_scalings

--- CombinePdfs/python/CPMixture.py
+++ CombinePdfs/python/CPMixture.py
@@ -5,13 +5,14 @@ class CPMixture(PhysicsModel):
 	def __init__(self):
 	  PhysicsModel.__init__(self)
           self.constrain_ggHYield  = False
-          self.sm_xs = 1.
-          self.ps_xs = 910./922.
+          self.lumi_scale = None
 
 	def setPhysicsOptions(self, physOptions):
                 for po in physOptions:
                   if po.startswith("constrain_ggHYield"):
                     self.constrain_ggHYield = True
+                  if po.startswith('lumiScale='):
+                    self.lumi_scale = po.replace('lumiScale=', '')
 
 	def doParametersOfInterest(self):
 		"""Create POI and other parameters, and define the POI set."""
@@ -31,22 +32,28 @@ class CPMixture(PhysicsModel):
                            'a_tilde' : 1.0,
                            'b_tilde' : 1.0
                          }
-			
+
 		self.modelBuilder.factory_('expr::a("cos(@0*{pi}/2)", alpha)'.format(pi=math.pi))
 		self.modelBuilder.factory_('expr::b("sin(@0*{pi}/2)", alpha)'.format(pi=math.pi))
 	        
 		self.modelBuilder.factory_('expr::sm_scaling("@0*@0-@0*@1*{a_tilde}/{b_tilde}", a, b)'.format(**maxmix))
 		self.modelBuilder.factory_('expr::ps_scaling("@1*@1-@0*@1*{b_tilde}/{a_tilde}", a, b)'.format(**maxmix))
                 self.modelBuilder.factory_('expr::mm_scaling("@0*@1/({a_tilde}*{b_tilde})", a, b)'.format(**maxmix))
-
-                self.modelBuilder.factory_('expr::xs_scaling("@0*@0*{sm_xs} +@1*@1*{ps_xs}", a, b)'.format(sm_xs=self.sm_xs,ps_xs=self.ps_xs))
                 
                 cps = ['sm_scaling', 'ps_scaling', 'mm_scaling'] 
                 for cp in cps:
                   
                   self.modelBuilder.factory_('expr::muF_{cp}("@0*@1", muF, {cp})'.format(cp=cp))
 
-                  self.modelBuilder.factory_('expr::muF_{cp}_xs_scaling("@0*@1*@2", muF, {cp}, xs_scaling)'.format(cp=cp)) 
+                if self.lumi_scale is not None:
+                  self.modelBuilder.factory_('expr::lumi_scaling("@0",{lumi_scale})'.format(lumi_scale=self.lumi_scale))
+                  self.modelBuilder.factory_('expr::muF_lumi_scaling("@0*@1", muF, lumi_scaling)'.format(cp=cp))
+                  self.modelBuilder.factory_('expr::muV_lumi_scaling("@0*@1", muV, lumi_scaling)'.format(cp=cp))
+                  for cp in cps:
+                    self.modelBuilder.factory_('expr::muF_{cp}_lumi_scaling("@0*@1*@2", muF, {cp}, lumi_scaling)'.format(cp=cp))
+
+
+  
 
 	def getYieldScale(self, bin, process):
 		if self.DC.isSignal[process]:
@@ -60,15 +67,20 @@ class CPMixture(PhysicsModel):
 			    scalings.append('ps_scaling')
 			  elif "f0p5" in process: 
                             scalings.append('mm_scaling') 
-                          if self.constrain_ggHYield: 
-                            scalings.append('xs_scaling')
-                        elif any(x in process for x in ['qqH','WH','ZH']): scalings.append('muV')
-                        
+                          else: return 1
+                        elif any(x in process for x in ['qqH','WH','ZH']): 
+                          scalings.append('muV')
+                        if self.lumi_scale is not None: scalings.append('lumi_scaling')                        
+
                         scaling = '_'.join(scalings)
                         print 'Scaling %s/%s as %s' % (bin, process, scaling)  
 			return scaling
 		
 		else: 
+                  if self.lumi_scale is not None: 
+                    print 'Scaling %s/%s as lumi_scaling' % (bin, process)
+                    return 'lumi_scaling'
+
                   print 'Scaling %s/%s as 1' % (bin, process)
                   return 1
 
