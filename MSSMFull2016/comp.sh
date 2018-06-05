528b4d75bdc8dc1001689e5e6859e5b42e0bf79c
mflechl <martin.flechl@cern.ch>
2018-06-05 13:38:52 +0200

    adding NP constraints evaluation to run.py adding comp.sh adding runall.sh

---
+++ MSSMFull2016/comp.sh
@@ -0,0 +1,1 @@
+scripts/projection/comp.sh
--- MSSMFull2016/scripts/projection/run.py
+++ MSSMFull2016/scripts/projection/run.py
@@ -20,7 +20,7 @@ def main(argv):
 
 
   parser = argparse.ArgumentParser(description='Steer the projection')
-  parser.add_argument('mode', nargs='?',                         default=['all'],  choices=[ 'all' , 'datacard' , 'ws' , 'limit' ],     help='Mode of operation')
+  parser.add_argument('mode', nargs='?',                         default=['all'],  choices=['all','datacard','ws','limit','mlfit','np'],     help='Mode of operation')
   parser.add_argument('--lumi',     dest='lumi',     type=float, default=baselumi,                                    help='Luminosity in fb-1')
   parser.add_argument('--nosyst',   dest='syst', nargs='?',      default=True, const=False,                           help='Flag to disable ystematics')
   parser.add_argument('--scale',    dest='scale',                default='none',   choices=[ 'all' , 'bbb', 'none' ], help='Scaling of uncertainties. Options: all, bbb, none')
@@ -51,6 +51,7 @@ def main(argv):
     args.mode.append('datacard')
     args.mode.append('ws')
     args.mode.append('limit')
+    args.mode.append('np')
 
   if 'datacard' in args.mode:
     create_output_dir()
@@ -101,6 +102,47 @@ def main(argv):
 
   ############################################## LIMIT
 
+  ############################################## NP
+  smass=700
+  os.chdir(basedir)
+  if 'mlfit' in args.mode:
+    #first the ml fit
+    mllog=basedir+'log_mlfit.txt'
+    pcall2='combineTool.py -M MaxLikelihoodFit --setPhysicsModelParameters r_ggH=0.0,r_bbH=0.0 --freezeNuisances r_bbH --setPhysicsModelParameterRanges r_ggH=-0.000001,0.000001 --setPhysicsModelParameterRanges r_bbH=-0.000001,0.000001 -d cmb/ws.root --redefineSignalPOIs r_ggH --there -m '+str(smass)+' --boundlist '+rundir+'/input/mssm_boundaries.json --minimizerTolerance 0.1 --minimizerStrategy 0 --name ".res" '+' &>'+mllog
+    make_pcall(pcall2)
+    make_pcall('mv cmb/mlfit.res.root cmb/mlfit.root','Renaming mlfit output files',2)
+
+    make_print( '########################################################################################' )
+    ret=os.popen('root -l cmb/mlfit.root <<<"fit_b->Print();" | grep "covariance matrix quality"').read().rstrip()
+    make_print( ret , 0 )
+    print_file(ret,mllog,'a')
+    make_print( '########################################################################################' )
+
+  if 'np' in args.mode:
+#    pcall='python '+CMSSW_BASE+'/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py cmb/mlfit.root -A -a --stol 0.99 --stol 0.99 --vtol 99. --vtol2 99. -f text --poi r_ggH --histogram nuisance_tests.root &> '+basedir+'/log_np.txt'
+    pcall='python '+CMSSW_BASE+'/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py cmb/mlfit.root -a --stol 0.99 --stol 0.99 --vtol 99. --vtol2 99. -f text --poi r_ggH --histogram nuisance_tests.root &> '+basedir+'/log_np.txt'
+    make_pcall(pcall,'Make NP texts... in dir: '+basedir)
+
+    exvar=' --exclude `cat ../combined.txt.cmb | awk {\'print $1\'} | grep _bin_ | head -c -1 | tr \'\\n\' \',\' `'
+    idir=basedir+'/cmb/impacts'
+    make_dir(idir)
+    os.chdir(idir)
+    make_print( '### Make NP plots... in dir: '+idir)
+    pcall4=[ 
+      'combineTool.py -t -1 -M Impacts -d ../ws.root -m '+str(smass)+' --setPhysicsModelParameters r_bbH=0,r_ggH=0 --setPhysicsModelParameterRanges r_ggH=-1.0,1.0 --doInitialFit --robustFit 1 --minimizerAlgoForMinos Minuit2,Migrad --redefineSignalPOIs r_ggH --freezeNuisances r_bbH'+exvar+' &>log1.txt',
+      'combineTool.py -t -1 -M Impacts -d ../ws.root -m '+str(smass)+' --setPhysicsModelParameters r_bbH=0,r_ggH=0 --setPhysicsModelParameterRanges r_ggH=-1.0,1.0 --robustFit 1 --doFits --minimizerAlgoForMinos Minuit2,Migrad  --redefineSignalPOIs r_ggH --freezeNuisances r_bbH --allPars --parallel 8'+exvar+' &>log2.txt',
+      'combineTool.py -M Impacts -d ../ws.root --allPars --redefineSignalPOIs r_ggH --exclude r_bbH -m '+str(smass)+' -o impacts.json'+exvar+' &>log3.txt',
+      'plotImpacts.py -i impacts.json -o impacts --transparent &>log4.txt',
+      'cp -p impacts.pdf ../',
+      ]
+    ll=[1,1,1,1,2]
+    for j,p4 in enumerate(pcall4):
+      make_pcall(p4,'',ll[j])
+
+    os.chdir(rundir)
+
+  ############################################## NP
+
 
 #def create_output_dir(webdir):
 def create_output_dir():
@@ -117,6 +159,13 @@ def create_output_dir():
 #            os.remove(webdir)
 #        os.symlink(mdir,webdir)
 
+def make_dir(d,msg=''):
+    if not msg=='':
+        make_print( '### '+msg )
+
+    if not os.path.exists(d):
+        make_print( ' >> mkdir -p '+d )
+        os.makedirs(d)
 
 def make_print(msg,level=1):
     global logf
@@ -127,6 +176,11 @@ def make_print(msg,level=1):
         print msg
 
     logf.write(msg+'\n')
+
+def print_file(text,fname,mode='w'):
+    f = open(fname, mode)
+    f.write(text)
+    f.close()
 
 def make_pcall(pcall,msg='',level=1,rep=0):
     if rep==0 and not msg=='': 
