26bae8ce6d664d9c3695fa5995a7e3e1ef6081d8
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2018-06-12 00:15:40 +0200

    Updating post fit plotting scripts for 2017

--- VH2017/scripts/makePostFitPlots2017.py
+++ VH2017/scripts/makePostFitPlots2017.py
@@ -1,23 +1,24 @@
 import os
 
 CHN_DICT_SR = {
-    "Wen": [["vhbb_Wen_1_13TeV2017","1-lepton (e)"]],
-    "Wmn": [["vhbb_Wmn_1_13TeV2017","1-lepton (#mu)"]],
-    "Zee": [["vhbb_Zee_1_13TeV2017","2-lepton (e), High p_{T} (V)"],["vhbb_Zee_2_13TeV2017","2-lepton (e), Low p_{T} (V)"]],
-    "Zmm": [["vhbb_Zmm_1_13TeV2017","2-lepton (#mu), High p_{T} (V)"],["vhbb_Zmm_2_13TeV2017","2-lepton (#mu), Low p_{T} (V)"]],
-    "Znn": [["vhbb_Znn_1_13TeV2017","0-lepton"]]
+    "Wen": [["vhbb_Wen_1_13TeV2017","1-lepton (e)","DNN output"]],
+    "Wmn": [["vhbb_Wmn_1_13TeV2017","1-lepton (#mu)","DNN output"]],
+    "Zee": [["vhbb_Zee_1_13TeV2017","2-lepton (e), High p_{T} (V)","BDT output"],["vhbb_Zee_2_13TeV2017","2-lepton (e), Low p_{T} (V)","BDT output"]],
+    "Zmm": [["vhbb_Zmm_1_13TeV2017","2-lepton (#mu), High p_{T} (V)","BDT output"],["vhbb_Zmm_2_13TeV2017","2-lepton (#mu), Low p_{T} (V)","BDT output"]],
+    "Znn": [["vhbb_Znn_1_13TeV2017","0-lepton","DNN output"]]
 }
 
 
 
 for MODE in ['prefit','postfit']:
-    for CHN in ['Wen','Wmn','Zee','Zmm','Znn']:
+    for CHN in ['Zee','Zmm','Znn','Wmn','Wen']:
         for i in range(0,len(CHN_DICT_SR[CHN])):
               LABEL = "%s" % CHN_DICT_SR[CHN][i][1]
               OUTNAME = "%s" % CHN_DICT_SR[CHN][i][0]
+              XAXIS = "%s" % CHN_DICT_SR[CHN][i][2]
               os.system(('./scripts/postFitPlot.py' \
-                  ' --file=shapes.root --ratio --extra_pad=0.53 --file_dir=%(OUTNAME)s ' \
+                  ' --file=shapes_sr.root --ratio --extra_pad=0.53 --file_dir=%(OUTNAME)s ' \
                   ' --ratio_range 0.4,1.6 --empty_bin_error --channel=%(CHN)s --blind' \
                   ' --outname %(OUTNAME)s --mode %(MODE)s --log_y --custom_y_range --y_axis_min "1E-2" '\
-                  ' --channel_label "%(LABEL)s"' % vars()))
+                  ' --channel_label "%(LABEL)s" --x_title "%(XAXIS)s" ' % vars()))
 
