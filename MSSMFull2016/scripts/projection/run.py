e9d922a38516c0418de7f73be0422d9af9b8d068
mflechl <martin.flechl@cern.ch>
2018-06-15 16:41:14 +0200

    extending run scripts: model-dependent limits

--- MSSMFull2016/scripts/projection/run.py
+++ MSSMFull2016/scripts/projection/run.py
@@ -23,10 +23,11 @@ def main(argv):
   global symdir,basedir,logfile
 
   parser = argparse.ArgumentParser(description='Steer the projection')
-  parser.add_argument('mode', nargs='?',                         default=['all'],  choices=['all','datacard','ws','limit','mlfit','np'],     help='Mode of operation')
+  parser.add_argument('mode', nargs='*',                         default=['all'],  choices=['all','datacard','ws','limit','mlfit','np'],     help='Mode of operation')
   parser.add_argument('--lumi',     dest='lumi',     type=float, default=baselumi,                                    help='Luminosity in fb-1')
   parser.add_argument('--nosyst',   dest='syst', nargs='?',      default=True, const=False,                           help='Flag to disable ystematics')
-  parser.add_argument('--scale',    dest='scale',                default='none',   choices=[ 'all' , 'bbb', 'none' ], help='Scaling of uncertainties. Options: all, bbb, none')
+  parser.add_argument('--scale',    dest='scale',                default='none',   choices=['all','bbb','none'],      help='Scaling of uncertainties. Options: all, bbb, none')
+  parser.add_argument('--model',    dest='model',                default='none',   choices=['mhmod','hmssm','none'],  help='Model-(in)dependent limits. Options: none, mhmod, hmssm')
   parser.add_argument('--loglevel', dest='loglevel', type=int,   default=1,                                           help='Verbosity, 0-essential, 1-most commands, 2-all commands')
   parser.add_argument('--outdir',   dest='outdir',               default=mdir,                                        help='root of output dir name (default: date/time)')
   parser.add_argument('--symdir',   dest='symdir',               default='latest/',                                   help='Symlink of output dir (change when running parallel!)')
@@ -37,6 +38,7 @@ def main(argv):
   syst=args.syst
   scale=args.scale
   loglevel=args.loglevel
+  model=args.model
 #  mdir+='_lumi-'+str(args.lumi)
   mdir=args.outdir+'_lumi-'+str(args.lumi)
   if not syst: mdir+='_nosyst'
@@ -64,13 +66,19 @@ def main(argv):
   make_print ( 'Projecting to lumi='+str(args.lumi)+' using a lumi scale of '+str(lumiscale)+' to scale the base lumi of '+str(baselumi) )
   make_print ( 'Applying systematics: '+str(syst) )
   make_print ( 'Scaling uncertainties: '+scale )
+  make_print ( 'Model: '+model )
   make_print ( 'Writing to '+physdir )
 
   os.chdir(rundir)
   ############################################## DATACARD
   if 'datacard' in args.mode:
-    pcall='MorphingMSSMFull2016 --output_folder='+symdir+' -m MH --manual_rebin=true --real_data=false ' +' &>'+basedir+'/log_datacard.txt'         #model-independent
-    make_pcall(pcall,'Producing data cards',0)
+    pcall_base='MorphingMSSMFull2016 --output_folder='+symdir+' --manual_rebin=true --real_data=false'
+    if model=='none':
+      pcall=pcall_base+' -m MH'
+    else:
+      pcall=pcall_base+' --remove_h=true'
+    pcall=pcall+' &>'+basedir+'/log_datacard.txt'
+    make_pcall(pcall,'Producing data cards for model '+model,0)
   ############################################## DATACARD
 
   ############################################## WORKSPACE
@@ -84,7 +92,17 @@ def main(argv):
     if scale=='bbb':
       scaleterm=scale_bbb
 
-    pcall='combineTool.py -M T2W -o ws.root --parallel 8 -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO \'"map=^.*/ggH.?$:r_ggH[0,0,200]"\' --PO \'"map=^.*/bbH$:r_bbH[0,0,200]"\' '+scaleterm+' -i output/'+symdir+'* &> '+basedir+'/log_ws.txt'
+#    pcall='combineTool.py -M T2W -o ws.root --parallel 8 -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO \'"map=^.*/ggH.?$:r_ggH[0,0,200]"\' --PO \'"map=^.*/bbH$:r_bbH[0,0,200]"\' '+scaleterm+' -i output/'+symdir+'* &> '+basedir+'/log_ws.txt'
+    pcall_base='combineTool.py -M T2W -o ws.root --parallel 8 '+scaleterm+' -i output/'+symdir+'*'
+
+    if model=='none':
+      pcall=pcall_base+' -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO \'"map=^.*/ggH.?$:r_ggH[0,0,200]"\' --PO \'"map=^.*/bbH$:r_bbH[0,0,200]"\''
+    elif model=='mhmod':
+      pcall=pcall_base+' -P CombineHarvester.CombinePdfs.MSSM:MSSM --PO filePrefix=$PWD/shapes/Models/ --PO modelFiles=13TeV,mhmodp_mu200_13TeV.root,1 --PO MSSM-NLO-Workspace=$PWD/shapes/Models/higgs_pt_v3_mssm_mode.root'
+    elif model=='hmssm':
+      pcall=pcall_base+' -P CombineHarvester.CombinePdfs.MSSM:MSSM --PO filePrefix=$PWD/shapes/Models/ --PO modelFiles=13TeV,hMSSM_13TeV.root,1 --PO MSSM-NLO-Workspace=$PWD/shapes/Models/higgs_pt_v3_mssm_mode.root'
+
+    pcall+=' &> '+basedir+'/log_ws.txt'
     make_pcall(pcall,'Producing workspace',0)
   ############################################## WORKSPACE
 
@@ -93,16 +111,35 @@ def main(argv):
     nfreeze='lumi'
     if not syst: nfreeze='all'
 
-    proc=[ 'ggH' , 'bbH' ]
-    make_pcall('echo Processes: '+str(proc)+' > '+basedir+'/log_lim.txt','',2)
-    for p in proc:
-      pcall1='combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic -t -1 --parallel 7 --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries-100.json --setPhysicsModelParameters r_ggH=0,r_bbH=0,lumi='+str(lumiscale)+' --freezeNuisances '+nfreeze+' --redefineSignalPOIs r_'+p+' -d output/'+symdir+'cmb/ws.root --there -n ".'+p+'" &>> '+basedir+'/log_lim.txt'
-      pcall2='combineTool.py -M CollectLimits output/'+symdir+'cmb/higgsCombine.'+p+'*.root --use-dirs -o "output/'+symdir+p+'.json" &>> '+basedir+'/log_lim.txt'
-      pcall3='python scripts/plotMSSMLimits.py --logy --logx --show exp output/'+symdir+p+'_cmb.json --cms-sub="Preliminary" -o output/'+symdir+p+'_cmb --process=\''+p[0:2]+'#phi\' --title-right="'+str(args.lumi)+' fb^{-1} (13 TeV)" --use-hig-17-020-style >> '+basedir+'/log_lim.txt'
-      make_pcall(pcall1,'Producing  limit for '+p,0)
-      make_pcall(pcall2,'Collecting limit for '+p,0)
-      make_pcall(pcall3,'Plotting   limit for '+p,0)
-
+    if model=='none':
+      proc=[ 'ggH' , 'bbH' ]
+      make_pcall('echo Processes: '+str(proc)+' > '+basedir+'/log_lim.txt','',2)
+      for p in proc:
+        pcall1='combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic -t -1 --parallel 7 --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries-100.json --setPhysicsModelParameters r_ggH=0,r_bbH=0,lumi='+str(lumiscale)+' --freezeNuisances '+nfreeze+' --redefineSignalPOIs r_'+p+' -d output/'+symdir+'cmb/ws.root --there -n ".'+p+'" &>> '+basedir+'/log_lim.txt'
+        pcall2='combineTool.py -M CollectLimits output/'+symdir+'cmb/higgsCombine.'+p+'*.root --use-dirs -o "output/'+symdir+p+'.json" &>> '+basedir+'/log_lim.txt'
+        pcall3='python scripts/plotMSSMLimits.py --logy --logx --show exp output/'+symdir+p+'_cmb.json --cms-sub="Preliminary" -o output/'+symdir+p+'_cmb --process=\''+p[0:2]+'#phi\' --title-right="'+str(args.lumi)+' fb^{-1} (13 TeV)" --use-hig-17-020-style >> '+basedir+'/log_lim.txt'
+        make_pcall(pcall1,'Producing  limit for '+p,0)
+        make_pcall(pcall2,'Collecting limit for '+p,0)
+        make_pcall(pcall3,'Plotting   limit for '+p,0)
+    else:
+      os.chdir(basedir)
+      dp='../../'
+      if model=='mhmod': js=dp+'./scripts/mssm_asymptotic_grid_mhmodp.json'
+      if model=='hmssm': js=dp+'./scripts/mssm_asymptotic_grid_hMSSM.json'
+      it=0
+      while True:
+        it+=1
+        pcall='combineTool.py -M AsymptoticGrid '+js+' -d cmb/ws.root --setPhysicsModelParameters lumi='+str(lumiscale)+' --freezeNuisances '+nfreeze+' -t -1 --parallel 8 &>> '+basedir+'/log_lim.txt'
+        make_pcall(pcall,'Producing  limit for '+model,0)
+        ret=os.popen('tail -1 '+basedir+'/log_lim.txt').read().rstrip()
+        if 'Replacing existing TGraph2D' in ret: 
+          break
+        if it>10: 
+          make_print( 'Stopping after restarting AsymptoticGrid for ten times. There is probably some problem, rather give up than ending up in an infinite loop' )
+          break
+        
+      make_print( 'python ../CombineTools/scripts/plotLimitGrid.py asymptotic_grid.root --scenario-label="m_{h}^{mod+} scenario" --output="mssm_mhmodp_cmb" --title-right="36.9 fb^{-1} (13 TeV)" --cms-sub="Preliminary" --contours="exp-2,exp-1,exp0,exp+1,exp+2,obs" --model_file=$PWD/shapes/Models/mhmodp_mu200_13TeV.root' )
+      make_print( '#Run the above on lxplus: /afs/cern.ch/work/m/mflechl/mssm_projection_tmp/CMSSW_7_4_7/src/CombineHarvester/MSSMFull2016' )
   ############################################## LIMIT
 
   ############################################## NP
@@ -137,7 +174,7 @@ def main(argv):
     pcall4=[ 
       'combineTool.py -t -1 -M Impacts -d ../ws.root -m '+str(smass)+' --setPhysicsModelParameters r_bbH=0,r_ggH=0,lumi='+str(lumiscale)+' --setPhysicsModelParameterRanges r_ggH=-1.0,1.0 --doInitialFit --robustFit 1 --minimizerAlgoForMinos Minuit2,Migrad --redefineSignalPOIs r_ggH --freezeNuisances '+nfreeze+exvar+' &>log1.txt',
       'combineTool.py -t -1 -M Impacts -d ../ws.root -m '+str(smass)+' --setPhysicsModelParameters r_bbH=0,r_ggH=0,lumi='+str(lumiscale)+' --setPhysicsModelParameterRanges r_ggH=-1.0,1.0 --robustFit 1 --doFits --minimizerAlgoForMinos Minuit2,Migrad  --redefineSignalPOIs r_ggH --freezeNuisances '+nfreeze+ ' --allPars --parallel 8'+exvar+' &>log2.txt',
-      'combineTool.py -M Impacts -d ../ws.root --allPars --redefineSignalPOIs r_ggH --exclude r_bbH,lumi -m '+str(smass)+' -o impacts.json'+exvar+' &>log3.txt',
+      'combineTool.py -M Impacts -d ../ws.root --allPars --redefineSignalPOIs r_ggH --setPhysicsModelParameters r_bbH=0,r_ggH=0,lumi='+str(lumiscale)+' --exclude r_bbH,lumi -m '+str(smass)+' -o impacts.json'+exvar+' &>log3.txt',
       'plotImpacts.py -i impacts.json -o impacts --transparent &>log4.txt',
       'cp -p impacts.pdf ../',
       ]
--- MSSMFull2016/scripts/projection/runall.sh
+++ MSSMFull2016/scripts/projection/runall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 lumis=( 36.9 300 3000 )
-odir="jun07"
+odir="jun15"
+#modes="all"
+modes="datacard ws limit"
+#extra=""
+extra="--model mhmod --symdir latest2/"
 
-#no syst
 for l in "${lumis[@]}"; do
     echo "###############################"
-    run.py --lumi $l --outdir $odir  --nosyst
+    run.py $modes --lumi $l --outdir $odir  --nosyst $extra
     echo "###############################"
-    run.py --lumi $l --outdir $odir
+    run.py $modes --lumi $l --outdir $odir $extra
     echo "###############################"
-    run.py --lumi $l --outdir $odir --scale all
+    run.py $modes --lumi $l --outdir $odir --scale all $extra
     echo "###############################"
-    run.py --lumi $l --outdir $odir --scale bbb
+    run.py $modes --lumi $l --outdir $odir --scale bbb $extra
 done
