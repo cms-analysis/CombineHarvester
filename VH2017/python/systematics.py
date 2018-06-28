2beb0d6188d0b08064a3d4066c816afd8a48246a
perrozzi <perrozzi@cern.ch>
2018-06-28 16:09:13 +0200

    sync to remove JER

--- VH2017/python/systematics.py
+++ VH2017/python/systematics.py
@@ -457,7 +457,7 @@ def AddSystematics2017(cb):
   cb.cp().AddSyst(cb,'CMS_bTagWeightDeepBcErr2_13TeV_pt4_eta1','shape',ch.SystMap()(1.0))
   cb.cp().AddSyst(cb,'CMS_bTagWeightDeepBcErr2_13TeV_pt4_eta2','shape',ch.SystMap()(1.0))
 
-  # EXCLUDE PROBLEMATIC NUISANCES for 2017 shapes shapes/AT/UNBLINDINGVHbb
+  # EXCLUDE PROBLEMATIC NUISANCES for 2017 shapes
   cb.FilterSysts(lambda x: 
                         x.channel() in ['Wen','Wmn'] and 
                         x.process() in ['s_Top','TT','Wj0b','Wj1b','Wj2b','Zj0b','Zj1b','Zj2b','VVHF','VVLF','WH_hbb','ZH_hbb'] and 
@@ -465,9 +465,9 @@ def AddSystematics2017(cb):
                         x.name() in 'CMS_scale_j_PileUpPtBB_13TeV'
                         )
   cb.FilterSysts(lambda x: 
-                        x.channel() in ['Znn'] and 
-                        x.process() in ['Zj0b'] and 
-                        x.bin_id() in [1,3] and 
+                        # x.channel() in ['Znn'] and 
+                        # x.process() in ['Zj0b'] and 
+                        # x.bin_id() in [1,3] and 
                         x.name() in 'CMS_res_j_13TeV'
                         )
   # cb.FilterSysts(lambda x: x.channel()=='Znn' and x.process()=='Zj1b' and x.bin_id()==7 and x.name()=='CMS_scale_j_RelativePtHF_13TeV')
@@ -833,3 +833,8 @@ def AddSystematics2016(cb):
   cb.cp().AddSyst(cb,'CMS_bTagWeightcErr2_13TeV_2016_pt4_eta0','shape',ch.SystMap()(1.0))
   cb.cp().AddSyst(cb,'CMS_bTagWeightcErr2_13TeV_2016_pt4_eta1','shape',ch.SystMap()(1.0))
   cb.cp().AddSyst(cb,'CMS_bTagWeightcErr2_13TeV_2016_pt4_eta2','shape',ch.SystMap()(1.0))
+
+  # EXCLUDE PROBLEMATIC NUISANCES for 2016 shapes
+  cb.FilterSysts(lambda x: 
+                        x.name() in 'CMS_res_j_13TeV_2016'
+                        )
