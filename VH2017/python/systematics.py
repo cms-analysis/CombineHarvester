dd7ddbb6e7605e65795775e615b71ec70ff6df56
perrozzi <perrozzi@cern.ch>
2018-06-28 22:52:52 +0200

    remove puweights problematic for 2017

--- VH2017/python/systematics.py
+++ VH2017/python/systematics.py
@@ -485,8 +485,23 @@ def AddSystematics2017(cb):
   cb.FilterSysts(lambda x: x.channel() in ['Zee'] and x.process() in 'Zj0b' and x.bin_id() in [2,3,4] and x.name()=='CMS_vhbb_puWeight')
   # Zj0b vhbb_Zmm_1_13TeV2017 CMS_vhbb_puWeight 
   # Zj0b vhbb_Zmm_2_13TeV2017 CMS_vhbb_puWeight 
-  # Zj0b vhbb_Zmm_4_13TeV2017 CMS_vhbb_puWeight
+  # Zj0b vhbb_Zmm_4_13TeV2017 CMS_vhbb_puWeight 
   cb.FilterSysts(lambda x: x.channel() in ['Zmm'] and x.process() in 'Zj0b' and x.bin_id() in [1,2,4] and x.name()=='CMS_vhbb_puWeight')
+  # TT vhbb_Wmn_7_13TeV2017 CMS_vhbb_puWeight 
+  # TT vhbb_Wen_7_13TeV2017 CMS_vhbb_puWeight 
+  cb.FilterSysts(lambda x: x.channel() in ['Wmn','Wen'] and x.process() in 'TT' and x.bin_id() in [7] and x.name()=='CMS_vhbb_puWeight')
+  # TT vhbb_Wmn_1_13TeV2017 CMS_vhbb_puWeight 
+  cb.FilterSysts(lambda x: x.channel() in ['Wmn'] and x.process() in 'TT' and x.bin_id() in [1] and x.name()=='CMS_vhbb_puWeight')
+  # Wj0b vhbb_Wen_3_13TeV2017 CMS_vhbb_puWeight 
+  # Wj0b vhbb_Wmn_3_13TeV2017 CMS_vhbb_puWeight 
+  cb.FilterSysts(lambda x: x.channel() in ['Wmn','Wen'] and x.process() in 'Wj0b' and x.bin_id() in [3] and x.name()=='CMS_vhbb_puWeight')
+  # Wj0b vhbb_Wen_6_13TeV2017 CMS_vhbb_puWeight 
+  cb.FilterSysts(lambda x: x.channel() in ['Wen'] and x.process() in 'Wj0b' and x.bin_id() in [6] and x.name()=='CMS_vhbb_puWeight')
+  # Wj0b vhbb_Znn_3_13TeV2017 CMS_vhbb_puWeight 
+  # Zj0b vhbb_Znn_3_13TeV2017 CMS_vhbb_puWeight 
+  cb.FilterSysts(lambda x: x.channel() in ['Znn'] and x.process() in ['Wj0b','Zj0b'] and x.bin_id() in [3] and x.name()=='CMS_vhbb_puWeight')
+  
+  
   
   
 ###########################################
