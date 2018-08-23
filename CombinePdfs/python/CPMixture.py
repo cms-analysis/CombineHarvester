19430427121982261cf56ceae82cb46016f3e7b6
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2018-08-23 14:15:33 +0100

    using MG for signal in physics model. atarting to add kappa options

--- CombinePdfs/python/CPMixture.py
+++ CombinePdfs/python/CPMixture.py
@@ -5,12 +5,15 @@ class CPMixture(PhysicsModel):
 	def __init__(self):
 	  PhysicsModel.__init__(self)
           self.constrain_ggHYield  = False
+          self.kappa = False
           self.lumi_scale = None
 
 	def setPhysicsOptions(self, physOptions):
                 for po in physOptions:
                   if po.startswith("constrain_ggHYield"):
                     self.constrain_ggHYield = True
+                  if po.startswith("kappa"):
+                    self.kappa = True
 
 	def doParametersOfInterest(self):
 		"""Create POI and other parameters, and define the POI set."""
@@ -18,10 +21,19 @@ class CPMixture(PhysicsModel):
 	        
                 poiNames = []
                 
-                self.modelBuilder.doVar('alpha[0,0,1]') 
-                poiNames.append('alpha')
-                self.modelBuilder.doVar('muF[1,0,4]')
-                poiNames.append('muF')
+                if not self.kappa:
+                  self.modelBuilder.doVar('alpha[0,0,1]') 
+                  poiNames.append('alpha')
+                  self.modelBuilder.doVar('muF[1,0,4]')
+                  poiNames.append('muF')
+                else:
+                  self.modelBuilder.doVar('kappaA[0,0,2]')
+                  self.modelBuilder.doVar('kappaH[1,0,2]')
+                  poiNames.append('kappaA')
+                  poiNames.append('kappaH') 
+                  self.modelBuilder.factory_('expr::alpha("3/2*@0/@1", kappaA, kappaH)')
+                  self.modelBuilder.factory_('expr::muF("cos(@0)*cos(@0)*@2*@2 + 9/4**sin(@0)*sin(@0)*@1*@1", alpha, kappaA, kappaH)')
+
 		self.modelBuilder.doVar('muV[1,0,4]')
                 self.modelBuilder.doVar('f[0,-1,1]')
 
@@ -30,10 +42,6 @@ class CPMixture(PhysicsModel):
                 params = {
                            'pi': math.pi, 
                            'ps_sm_xs_ratio' : 2.25,
-                           'ggh_sigma1' : 0.399064, 
-                           'ggh_sigma3' : 0.393961,
-                           'a1_mm' : 1.0,
-                           'a3_mm' : 1.0062,
                            'qqh_sigma1' : 0.2371314,
                            'qqh_sigma3' : 2.6707,
                            'a1_qqh_mm' : 1.0,
@@ -41,10 +49,10 @@ class CPMixture(PhysicsModel):
                          }
 
                 self.modelBuilder.factory_('expr::a1("sqrt(@0)*cos(@1*{pi}/2)", muF, alpha)'.format(**params))
-                self.modelBuilder.factory_('expr::a3("sqrt(@0)*sin(@1*{pi}/2)*sqrt({ggh_sigma1}/{ggh_sigma3})", muF, alpha)'.format(**params))
-                self.modelBuilder.factory_('expr::sm_scaling("@0*@0 - @0*@1*{a1_mm}/{a3_mm}", a1, a3)'.format(**params))
-                self.modelBuilder.factory_('expr::ps_scaling("@1*@1 - @0*@1*{a3_mm}/{a1_mm}", a1, a3)'.format(**params))
-                self.modelBuilder.factory_('expr::mm_scaling("@0*@1/({a1_mm}*{a3_mm})", a1, a3)'.format(**params))
+                self.modelBuilder.factory_('expr::a3("sqrt(@0)*sin(@1*{pi}/2)", muF, alpha)'.format(**params))
+                self.modelBuilder.factory_('expr::sm_scaling("@0*@0 - @0*@1", a1, a3)')
+                self.modelBuilder.factory_('expr::ps_scaling("@1*@1 - @0*@1", a1, a3)')
+                self.modelBuilder.factory_('expr::mm_scaling("2*@0*@1", a1, a3)')
  
                 # JHU samples have constant XS that does not depend on alpha. If the constrain_ggHYield option is set then add a scaling as a function of alpha -> XS = cos^2(alpha) + (3/2)^2*sin^2(alpha) to match MG5 XS                
                 if self.constrain_ggHYield:
@@ -69,12 +77,15 @@ class CPMixture(PhysicsModel):
 		if 'ggH' in process and 'hww' not in process: 
                   if self.constrain_ggHYield:
                     scalings.append('xs_sf')
-                  if "ggHsm" in process:
+                  if "ggHsm" in process or "ggH2jsm" in process:
     		    scalings.append('sm_scaling')
-		  elif "ggHps" in process:
+		  elif "ggHps" in process or "ggH2jps" in process:
 		    scalings.append('ps_scaling')
-		  elif "ggHmm" in process: 
-                    scalings.append('mm_scaling') 
+		  elif "ggHmm" in process or "ggH2jmm" in process:
+                    scalings.append('mm_scaling')
+                  elif 'ggH_ph' in process:
+                    scalings.append('muF')  
+                   
 
                 if any(x in process for x in ['WH','ZH']) and not 'hww' in process:
                     scalings.append('muV')
