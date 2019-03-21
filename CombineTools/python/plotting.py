cb197eb4a7212c1368239355114e8a6e2710e3c5
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2019-03-21 12:39:38 +0100

    Saving various plotting scripts
    Conflicts:
    	CombineTools/python/plotting.py
    	VH2017/bin/VHbbSoverBPlot.cpp
    	VH2017/scripts/plotCCC.py
    	VH2017/scripts/plotSoverBorderedFromcpp.py

--- CombineTools/python/plotting.py
+++ CombineTools/python/plotting.py
@@ -1528,7 +1528,7 @@ def DrawVerticalBand(pad, box, x1, x2):
     box.DrawBox(x1, ymin, x2, ymax)
 
 
-def DrawTitle(pad, text, align, textOffset=0.2):
+def DrawTitle(pad, text, align, textOffset=0.2,textSize=0.6):
     pad_backup = R.gPad
     pad.cd()
     t = pad.GetTopMargin()
@@ -1539,8 +1539,6 @@ def DrawTitle(pad, text, align, textOffset=0.2):
         (float(pad.GetWw()) * pad.GetAbsWNDC())
     if pad_ratio < 1.:
         pad_ratio = 1.
-
-    textSize = 0.6
 
     latex = R.TLatex()
     latex.SetNDC()
