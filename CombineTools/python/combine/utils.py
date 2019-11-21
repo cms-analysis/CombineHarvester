5fa9f6348bd840fbbde7e5b5fc178c88fd7bcec6
Heshy Roskes <hroskes@jhu.edu>
2019-11-21 09:30:14 -0500

    compatibility for older combine version

--- CombineTools/python/combine/utils.py
+++ CombineTools/python/combine/utils.py
@@ -1,7 +1,12 @@
 import ROOT
 import re
 
-from HiggsAnalysis.CombinedLimit.RooAddPdfFixer import FixAll
+try:
+    from HiggsAnalysis.CombinedLimit.RooAddPdfFixer import FixAll
+except ImportError:
+    #compatibility for combine version earlier than https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit/tree/2d172ef50fccdfbbc2a499ac8e47bba2d667b95a
+    #can delete in a few months
+    def FixAll(workspace): pass
 
 def split_vals(vals, fmt_spec=None):
     """Converts a string '1:3|1,4,5' into a list [1, 2, 3, 4, 5]"""
