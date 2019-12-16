143bdabbaf8cd1724793c8c02169ce329b980cc5
Olena Hlushchenko <greyxray@gmail.com>
2019-12-16 15:42:33 +0100

    impacts: add option for automatic page break by magnituted, automatic margine update when 10^X appear

--- CombineTools/scripts/plotImpacts.py
+++ CombineTools/scripts/plotImpacts.py
@@ -27,6 +27,7 @@ parser.add_argument('--blind', action='store_true', help='Do not print best fit 
 parser.add_argument('--color-groups', default=None, help='Comma separated list of GROUP=COLOR')
 parser.add_argument("--pullDef",  default=None, help="Choose the definition of the pull, see HiggsAnalysis/CombinedLimit/python/calculate_pulls.py for options")
 parser.add_argument('--POI', default=None, help='Specify a POI to draw')
+parser.add_argument('--auto-page', action='store_true', default=False, help='Automatic number of pages')
 args = parser.parse_args()
 
 if args.transparent:
@@ -118,6 +119,334 @@ if args.color_groups is not None:
         color_group_hists[name] = ROOT.TH1F()
         plot.Set(color_group_hists[name], FillColor=col, Title=name)
 
+if args.auto_page:
+    # prepare the results to plot
+    per_page_numbers = []
+    all_results = {
+        'pull': [],
+        'pull_hi': [],
+        'pull_lo': [],
+        'text_entries': [],
+        'g_impacts_hi_err': [],
+        'g_impacts_lo_err': [],
+        'max_impact': [],
+    }
+    # keydict = dict(zip(all_results['max_impact'], Y))
+    neg_sum = 0
+    pos_sum = 0
+    n_params = len(data['params'])
+    for p in xrange(n_params):
+        pre = data['params'][p]['prefit']
+        fit = data['params'][p]['fit']
+        tp = data['params'][p]['type']
+        seen_types.add(tp)
+
+        pre_err_hi = (pre[2] - pre[1])
+        pre_err_lo = (pre[1] - pre[0])
+
+        if externalPullDef:
+            fit_err_hi = (fit[2] - fit[1])
+            fit_err_lo = (fit[1] - fit[0])
+            pull, pull_hi, pull_lo = CP.returnPullAsym(args.pullDef,fit[1],pre[1],fit_err_hi,pre_err_hi,fit_err_lo,pre_err_lo)
+        else:
+            pull = fit[1] - pre[1]
+            pull = (pull/pre_err_hi) if pull >= 0 else (pull/pre_err_lo)
+            pull_hi = fit[2] - pre[1]
+            pull_hi = (pull_hi/pre_err_hi) if pull_hi >= 0 else (pull_hi/pre_err_lo)
+            pull_hi = pull_hi - pull
+            pull_lo = fit[0] - pre[1]
+            pull_lo = (pull_lo/pre_err_hi) if pull_lo >= 0 else (pull_lo/pre_err_lo)
+            pull_lo =  pull - pull_lo
+
+        all_results['pull'].append(pull)
+        all_results['pull_lo'].append(pull_lo)
+        all_results['pull_hi'].append(pull_hi)
+
+        imp = data['params'][p][POI]
+        all_results['g_impacts_lo_err'].append(imp[1] - imp[0])
+        all_results['g_impacts_hi_err'].append(imp[2] - imp[1])
+        if imp[1] - imp[0] > 0 :
+            pos_sum += pow(imp[1] - imp[0], 2)
+        else:
+            neg_sum += pow(imp[1] - imp[0], 2)
+        if imp[2] - imp[1] > 0 :
+            pos_sum += pow(imp[2] - imp[1], 2)
+        else:
+            neg_sum += pow(imp[2] - imp[1], 2)
+        all_results['max_impact'].append(max(abs(imp[1] - imp[0]), abs(imp[2] - imp[1])))
+
+    # optimal page-break
+    import math
+    print 'tot. pos. uncertainty from impacts:', math.sqrt(pos_sum)
+    print 'tot. neg. uncertainty from impacts:', math.sqrt(neg_sum)
+
+    max_impact = sorted(all_results['max_impact'], reverse=True)
+    last_on_page = []
+    nonpage=0
+    # n = (len(data['params']) / show) #int(math.log10(x))
+    for i in range(0, len(max_impact) - 1):
+        if i + 1 == len(max_impact) - 1:
+            break
+        if nonpage >= show or int(math.log10(max_impact[i + 1])) != int(math.log10(max_impact[i])): #max_impact[i + 1] < max_impact[i]/10:
+            if max_impact[i + 1] < max_impact[i]/10:
+                print '!break(scale):', max_impact[i + 1] ,"<", max_impact[i]/10, '(', int(math.log10(max_impact[i + 1])), int(math.log10(max_impact[i])), ')'
+            else:
+                print '!break(nmax):', nonpage ,">=", show
+            last_on_page.append(i)
+            nonpage = 0
+            continue
+        else:
+            print 'nobreak:', max_impact[i + 1] ,">=", max_impact[i]/10
+        nonpage += 1
+    last_on_page.append(len(max_impact))
+    n = len(last_on_page)
+    # update margin when 10^x appear
+    updatemargin = False
+    if int(math.log10(min(all_results['max_impact'])))<=2 :
+        print "changing margin"
+        updatemargin = True
+    # print max_impact
+    # print 'nparam:', len(data['params'])
+    # print last_on_page ; exit(1)
+
+    # produce plot
+    for page in xrange(n):
+        canv = ROOT.TCanvas(args.output, args.output)
+        if page == 0:
+            start = 0
+            if last_on_page[0] == 0:
+                n_params = len(data['params'][0:1])
+                pdata = data['params'][0:1]
+                last_on_page[0] = 1
+            else:
+                n_params = len(data['params'][0:last_on_page[page]])
+                pdata = data['params'][0:last_on_page[page]]
+        else:
+            start = last_on_page[page - 1]
+            n_params = len(data['params'][last_on_page[page - 1]:last_on_page[page]])
+            pdata = data['params'][last_on_page[page - 1]:last_on_page[page]]
+        # show = len(n_params)
+
+        # n_params = len(data['params'][show * page:show * (page + 1)])
+        # pdata = data['params'][show * page:show * (page + 1)]
+        print '>> Doing page %i, have %i parameters, indexed [%i, %i)' % (page, n_params, start, last_on_page[page])
+
+        boxes = []
+        for i in xrange(n_params):
+            y1 = ROOT.gStyle.GetPadBottomMargin()
+            y2 = 1. - ROOT.gStyle.GetPadTopMargin()
+            h = (y2 - y1) / float(n_params)
+            y1 = y1 + float(i) * h
+            y2 = y1 + h
+            box = ROOT.TPaveText(0, y1, 1, y2, 'NDC')
+            plot.Set(box, TextSize=0.02, BorderSize=0, FillColor=0, TextAlign=12, Margin=0.005)
+            if i % 2 == 0:
+                box.SetFillColor(18)
+            box.AddText('%i' % (n_params - i + start))
+            box.Draw()
+            boxes.append(box)
+
+        # Crate and style the pads
+        if args.checkboxes:
+            pads = plot.MultiRatioSplitColumns([0.54, 0.24], [0., 0.], [0., 0.])
+            pads[2].SetGrid(1, 0)
+        else:
+            pads = plot.MultiRatioSplitColumns([0.7], [0.], [0.])
+        pads[0].SetGrid(1, 0)
+        pads[0].SetTickx(1)
+        pads[1].SetGrid(1, 0)
+        pads[1].SetTickx(1)
+        if updatemargin:
+            pads[1].SetRightMargin(pads[1].GetRightMargin() + 0.025)
+
+        h_pulls = ROOT.TH2F("pulls", "pulls", 6, -2.9, 2.9, n_params, 0, n_params)
+        g_pulls = ROOT.TGraphAsymmErrors(n_params)
+        g_impacts_hi = ROOT.TGraphAsymmErrors(n_params)
+        g_impacts_lo = ROOT.TGraphAsymmErrors(n_params)
+        g_check = ROOT.TGraphAsymmErrors()
+        g_check_i = 0
+
+        max_impact = 0.
+
+        text_entries = []
+        redo_boxes = []
+        for p in xrange(n_params):
+            i = n_params - (p + 1)
+            pre = pdata[p]['prefit']
+            fit = pdata[p]['fit']
+            tp = pdata[p]['type']
+            seen_types.add(tp)
+            if pdata[p]['type'] != 'Unconstrained':
+                pre_err_hi = (pre[2] - pre[1])
+                pre_err_lo = (pre[1] - pre[0])
+
+                if externalPullDef:
+                    fit_err_hi = (fit[2] - fit[1])
+                    fit_err_lo = (fit[1] - fit[0])
+                    pull, pull_hi, pull_lo = CP.returnPullAsym(args.pullDef,fit[1],pre[1],fit_err_hi,pre_err_hi,fit_err_lo,pre_err_lo)
+                else:
+                    pull = fit[1] - pre[1]
+                    pull = (pull/pre_err_hi) if pull >= 0 else (pull/pre_err_lo)
+                    pull_hi = fit[2] - pre[1]
+                    pull_hi = (pull_hi/pre_err_hi) if pull_hi >= 0 else (pull_hi/pre_err_lo)
+                    pull_hi = pull_hi - pull
+                    pull_lo = fit[0] - pre[1]
+                    pull_lo = (pull_lo/pre_err_hi) if pull_lo >= 0 else (pull_lo/pre_err_lo)
+                    pull_lo =  pull - pull_lo
+
+                g_pulls.SetPoint(i, pull, float(i) + 0.5)
+                g_pulls.SetPointError(
+                    i, pull_lo, pull_hi, 0., 0.)
+            else:
+                # Hide this point
+                g_pulls.SetPoint(i, 0., 9999.)
+                y1 = ROOT.gStyle.GetPadBottomMargin()
+                y2 = 1. - ROOT.gStyle.GetPadTopMargin()
+                x1 = ROOT.gStyle.GetPadLeftMargin()
+                h = (y2 - y1) / float(n_params)
+                y1 = y1 + ((float(i)+0.5) * h)
+                x1 = x1 + (1 - pads[0].GetRightMargin() -x1)/2.
+                s_nom, s_hi, s_lo = GetRounded(fit[1], fit[2] - fit[1], fit[1] - fit[0])
+                text_entries.append((x1, y1, '%s^{#plus%s}_{#minus%s}' % (s_nom, s_hi, s_lo)))
+                redo_boxes.append(i)
+            g_impacts_hi.SetPoint(i, 0, float(i) + 0.5)
+            g_impacts_lo.SetPoint(i, 0, float(i) + 0.5)
+            if args.checkboxes:
+                pboxes = pdata[p]['checkboxes']
+                for pbox in pboxes:
+                    cboxes.index(pbox)
+                    g_check.SetPoint(g_check_i, cboxes.index(pbox) + 0.5, float(i) + 0.5)
+                    g_check_i += 1
+            imp = pdata[p][POI]
+            g_impacts_hi.SetPointError(i, 0, imp[2] - imp[1], 0.5, 0.5)
+            g_impacts_lo.SetPointError(i, imp[1] - imp[0], 0, 0.5, 0.5)
+            max_impact = max(
+                max_impact, abs(imp[1] - imp[0]), abs(imp[2] - imp[1]))
+            col = colors.get(tp, 2)
+            if args.color_groups is not None and len(pdata[p]['groups']) >= 1:
+                for p_grp in pdata[p]['groups']:
+                    if p_grp in color_groups:
+                        col = color_groups[p_grp]
+                        break
+            h_pulls.GetYaxis().SetBinLabel(
+                i + 1, ('#color[%i]{%s}'% (col, Translate(pdata[p]['name'], translate))))
+
+        # Style and draw the pulls histo
+        if externalPullDef:
+            plot.Set(h_pulls.GetXaxis(), TitleSize=0.04, LabelSize=0.03, Title=CP.returnTitle(args.pullDef))
+        else:
+            plot.Set(h_pulls.GetXaxis(), TitleSize=0.04, LabelSize=0.03, Title='(#hat{#theta}-#theta_{0})/#Delta#theta')
+
+        plot.Set(h_pulls.GetYaxis(), LabelSize=args.label_size, TickLength=0.0)
+        h_pulls.GetYaxis().LabelsOption('v')
+        h_pulls.Draw()
+
+        for i in redo_boxes:
+            newbox = boxes[i].Clone()
+            newbox.Clear()
+            newbox.SetY1(newbox.GetY1()+0.005)
+            newbox.SetY2(newbox.GetY2()-0.005)
+            newbox.SetX1(ROOT.gStyle.GetPadLeftMargin()+0.001)
+            newbox.SetX2(0.7-0.001)
+            newbox.Draw()
+            boxes.append(newbox)
+        latex = ROOT.TLatex()
+        latex.SetNDC()
+        latex.SetTextFont(42)
+        latex.SetTextSize(0.02)
+        latex.SetTextAlign(22)
+        for entry in text_entries:
+            latex.DrawLatex(*entry)
+
+        # Go to the other pad and draw the impacts histo
+        pads[1].cd()
+        if max_impact == 0.: max_impact = 1E-6  # otherwise the plotting gets screwed up
+        h_impacts = ROOT.TH2F(
+            "impacts", "impacts", 6, -max_impact * 1.1, max_impact * 1.1, n_params, 0, n_params)
+        plot.Set(h_impacts.GetXaxis(), LabelSize=0.03, TitleSize=0.04, Ndivisions=505, Title=
+            '#Delta#hat{%s}' % (Translate(POI, translate)))
+        plot.Set(h_impacts.GetYaxis(), LabelSize=0, TickLength=0.0)
+        h_impacts.Draw()
+
+        if args.checkboxes:
+            pads[2].cd()
+            h_checkboxes = ROOT.TH2F(
+                "checkboxes", "checkboxes", len(cboxes), 0, len(cboxes), n_params, 0, n_params)
+            for i, cbox in enumerate(cboxes):
+                h_checkboxes.GetXaxis().SetBinLabel(i+1, Translate(cbox, translate))
+            plot.Set(h_checkboxes.GetXaxis(), LabelSize=0.03, LabelOffset=0.002)
+            h_checkboxes.GetXaxis().LabelsOption('v')
+            plot.Set(h_checkboxes.GetYaxis(), LabelSize=0, TickLength=0.0)
+            h_checkboxes.Draw()
+            # g_check.SetFillColor(ROOT.kGreen)
+            g_check.Draw('PSAME')
+
+        # Back to the first pad to draw the pulls graph
+        pads[0].cd()
+        plot.Set(g_pulls, MarkerSize=0.8, LineWidth=2)
+        g_pulls.Draw('PSAME')
+
+        # And back to the second pad to draw the impacts graphs
+        pads[1].cd()
+        alpha = 0.7
+
+        lo_color = {
+            'default': 38,
+            'hesse': ROOT.kOrange - 3,
+            'robust': ROOT.kGreen + 1
+        }
+        hi_color = {
+            'default': 46,
+            'hesse': ROOT.kBlue,
+            'robust': ROOT.kAzure - 5
+        }
+        method = 'default'
+        if 'method' in data and data['method'] in lo_color:
+            method = data['method']
+        g_impacts_hi.SetFillColor(plot.CreateTransparentColor(hi_color[method], alpha))
+        g_impacts_hi.Draw('2SAME')
+        g_impacts_lo.SetFillColor(plot.CreateTransparentColor(lo_color[method], alpha))
+        g_impacts_lo.Draw('2SAME')
+        pads[1].RedrawAxis()
+
+        legend = ROOT.TLegend(0.02, 0.02, 0.40, 0.06, '', 'NBNDC')
+        legend.SetNColumns(3)
+        legend.AddEntry(g_pulls, 'Pull', 'LP')
+        legend.AddEntry(g_impacts_hi, '+1#sigma Impact', 'F')
+        legend.AddEntry(g_impacts_lo, '-1#sigma Impact', 'F')
+        legend.Draw()
+
+        leg_width = pads[0].GetLeftMargin() - 0.01
+        if args.color_groups is not None:
+            legend2 = ROOT.TLegend(0.01, 0.94, leg_width, 0.99, '', 'NBNDC')
+            legend2.SetNColumns(2)
+            for name, h in color_group_hists.iteritems():
+                legend2.AddEntry(h, Translate(name, translate), 'F')
+            legend2.Draw()
+        elif len(seen_types) > 1:
+            legend2 = ROOT.TLegend(0.01, 0.94, leg_width, 0.99, '', 'NBNDC')
+            legend2.SetNColumns(2)
+            for name, h in color_hists.iteritems():
+                if name == 'Unrecognised': continue
+                legend2.AddEntry(h, name, 'F')
+            legend2.Draw()
+
+        plot.DrawCMSLogo(pads[0], 'CMS', args.cms_label, 0, 0.25, 0.00, 0.00)
+        s_nom, s_hi, s_lo = GetRounded(POI_fit[1], POI_fit[2] - POI_fit[1], POI_fit[1] - POI_fit[0])
+        if not args.blind:
+            plot.DrawTitle(pads[1], '#hat{%s} = %s^{#plus%s}_{#minus%s}%s' % (
+                Translate(POI, translate), s_nom, s_hi, s_lo,
+                '' if args.units is None else ' '+args.units), 3, 0.27)
+        extra = ''
+        if page == 0:
+            extra = '('
+        if page == n - 1:
+            extra = ')'
+        canv.Print('.pdf%s' % extra)
+        canv.Print('.png%s' % extra)
+
+    exit(0)
+
 for page in xrange(n):
     canv = ROOT.TCanvas(args.output, args.output)
     n_params = len(data['params'][show * page:show * (page + 1)])
@@ -158,6 +487,7 @@ for page in xrange(n):
     g_check_i = 0
 
     max_impact = 0.
+    min_impact = 999.
 
     text_entries = []
     redo_boxes = []
@@ -213,6 +543,8 @@ for page in xrange(n):
         g_impacts_lo.SetPointError(i, imp[1] - imp[0], 0, 0.5, 0.5)
         max_impact = max(
             max_impact, abs(imp[1] - imp[0]), abs(imp[2] - imp[1]))
+        min_impact = min(
+            min_impact, abs(imp[1] - imp[0]), abs(imp[2] - imp[1]))
         col = colors.get(tp, 2)
         if args.color_groups is not None and len(pdata[p]['groups']) >= 1:
             for p_grp in pdata[p]['groups']:
@@ -221,6 +553,9 @@ for page in xrange(n):
                     break
         h_pulls.GetYaxis().SetBinLabel(
             i + 1, ('#color[%i]{%s}'% (col, Translate(pdata[p]['name'], translate))))
+
+    if int(math.log10(min_impact)) <= 2:
+        pads[1].SetRightMargin(pads[1].GetRightMargin() + 0.025)
 
     # Style and draw the pulls histo
     if externalPullDef:
@@ -334,3 +669,4 @@ for page in xrange(n):
     if page == n - 1:
         extra = ')'
     canv.Print('.pdf%s' % extra)
+    canv.Print('.png%s' % extra)
