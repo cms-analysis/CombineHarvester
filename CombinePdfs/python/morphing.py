00c272ac645804a34a98bab3b20d5ae9296ff530
Nick Smith <nick.smith@cern.ch>
2022-06-07 11:18:51 -0500

    Migrate BuildRooMorphing to cppyy as well

--- CombinePdfs/python/morphing.py
+++ CombinePdfs/python/morphing.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
-try:
-    from libCombineHarvesterCombinePdfs import *
-except ImportError:
-    raise ImportError(
-        'Module is missing: you need to compile the C++ -> python shared library by running make')
+import cppyy
+
+cppyy.include("CombineHarvester/CombinePdfs/interface/MorphFunctions.h")
 
 
 def BuildRooMorphing(ws, cb, bin, process, mass_var, norm_postfix='norm', allow_morph=True, verbose=False, force_template_limit=False, file=None):
-    return BuildRooMorphingX(ws, cb, bin, process, mass_var, norm_postfix, allow_morph, verbose, force_template_limit, file)
+    return cppyy.gbl.BuildRooMorphing(ws, cb, bin, process, mass_var, norm_postfix, allow_morph, verbose, force_template_limit, file);
